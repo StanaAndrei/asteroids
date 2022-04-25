ad7fc333e86c5c6f52dceadd80812ec9ff286f5a
StanaAndrei <stadey33@gmail.com>
2022-04-25 11:58:04 +0300

    finished collision

--- CollisionChecker.js
+++ CollisionChecker.js
@@ -17,9 +17,15 @@ class CollisionChecker {
     asteroidHitShip(asteroid, ship, p5context) {
         const { p1, p2, p3 } = ship.getVertices(p5context);
         let hit = false;
+
+        hit |= Geometry.segInCircle(p1.x, p1.y, p2.x, p2.y, asteroid.x, asteroid.y, asteroid.diam / 2);
+        hit |= Geometry.segInCircle(p1.x, p1.y, p3.x, p3.y, asteroid.x, asteroid.y, asteroid.diam / 2);
+        hit |= Geometry.segInCircle(p2.x, p2.y, p3.x, p3.y, asteroid.x, asteroid.y, asteroid.diam / 2);
+
         hit |= Geometry.pointInCircle(p1.x, p1.y, asteroid.x, asteroid.y, asteroid.diam / 2);
         hit |= Geometry.pointInCircle(p2.x, p2.y, asteroid.x, asteroid.y, asteroid.diam / 2);
         hit |= Geometry.pointInCircle(p3.x, p3.y, asteroid.x, asteroid.y, asteroid.diam / 2);
+
         return hit;
     }
 }
--- Geometry.js
+++ Geometry.js
@@ -1,11 +1,35 @@
 export default class Geometry {
+    static triangleArea(m) {
+        for (let i = 0; i < 3; i++) {
+            m[i].push(1);
+        }
+        let det = 0;
+    
+        det += m[0][0] * m[1][1] * m[2][2];
+        det += m[0][2] * m[1][0] * m[2][1];
+        det += m[2][0] * m[0][1] * m[1][2];
+    
+        det -= m[0][2] * m[1][1] * m[2][0];
+        det -= m[0][0] * m[1][2] * m[2][1];
+        det -= m[2][2] * m[0][1] * m[1][0];
+    
+        return Math.abs(det) / 2;
+    }
+
     static pointInCircle(px, py, cx, cy, r) {
         return (Math.pow(r, 2) >=
             Math.pow(px - cx, 2) + Math.pow(py - cy, 2)
         )
     }
 
-    static lineInCircle(a, b, c, x, y, radius) {
-
+    static segInCircle(x1, y1, x2, y2, x, y, radius) {
+        if (!((Math.min(x1, x2) <= x && x <= Math.max(x1, x2)) && (Math.min(y1, y2) <= y && y <= Math.max(y1, y2)))) {
+            return false;
+        }
+        const areaOfTr = Geometry.triangleArea([[x1, y1], [x2, y2], [x, y]]);
+        const base = Math.hypot(x2 - x1, y2 - y1);
+        const dis = 2 * areaOfTr / base;
+        //console.log(dis);
+        return dis <= radius;
     }
 }
